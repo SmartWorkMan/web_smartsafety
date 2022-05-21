f5fb228cb5afbc54592821ae2636e5a173c7e50c
yl <yang_le-e@qq.com>
2022-05-21 16:03:50 +0800

    创建工厂页面，维保管理页面

---
+++ src/view/createFactory/index.vue
@@ -0,0 +1,345 @@
+<template>
+  <div>
+    <div class="gva-table-box">
+      <div class="gva-btn-list">
+        <el-button size="small" type="primary" icon="plus" @click="addUser">新建工厂</el-button>
+      </div>
+      <el-table
+        :data="tableData"
+        row-key="ID"
+      >
+        <el-table-column align="left" label="ID" min-width="50" prop="ID" />
+        <el-table-column align="left" label="工厂名" min-width="150" prop="userName" />
+        <el-table-column align="left" label="工厂描述" min-width="150" prop="nickName" />
+
+
+        <el-table-column label="操作" min-width="250" fixed="right">
+          <template #default="scope">
+            <!-- <el-popover v-model:visible="scope.row.visible" placement="top" width="160">
+              <p>确定要删除此用户吗</p>
+              <div style="text-align: right; margin-top: 8px;">
+                <el-button size="small" type="text" @click="scope.row.visible = false">取消</el-button>
+                <el-button type="primary" size="small" @click="deleteUserFunc(scope.row)">确定</el-button>
+              </div>
+              <template #reference>
+                <el-button type="text" icon="delete" size="small">删除</el-button>
+              </template>
+            </el-popover> -->
+            <el-button type="text" icon="edit" size="small" @click="openEdit(scope.row)">编辑</el-button>
+            <!-- <el-button type="text" icon="magic-stick" size="small" @click="resetPasswordFunc(scope.row)">重置密码</el-button> -->
+          </template>
+        </el-table-column>
+
+      </el-table>
+      <div class="gva-pagination">
+        <el-pagination
+          :current-page="page"
+          :page-size="pageSize"
+          :page-sizes="[10, 30, 50, 100]"
+          :total="total"
+          layout="total, sizes, prev, pager, next, jumper"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </div>
+    </div>
+    <el-dialog
+      v-model="addUserDialog"
+      custom-class="user-dialog"
+      title="工厂信息"
+      :show-close="false"
+      :close-on-press-escape="false"
+      :close-on-click-modal="false"
+    >
+      <div style="height:60vh;overflow:auto;padding:0 12px;">
+        <el-form ref="userForm" :rules="rules" :model="userInfo" label-width="80px">
+          
+          <el-form-item label="工厂名" prop="nickName">
+            <el-input v-model="userInfo.nickName" />
+          </el-form-item>
+          <el-form-item label="工厂名前缀" prop="phone">
+            <el-input v-model="userInfo.phone" placeholder="用于创建为保管理员及工厂管理员账号的前缀"/>
+          </el-form-item>
+
+        </el-form>
+
+      </div>
+
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button size="small" @click="closeAddUserDialog">取 消</el-button>
+          <el-button size="small" type="primary" @click="enterAddUserDialog">确 定</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateFactory',
+}
+</script>
+
+<script setup>
+
+import {
+  getUserList,
+  setUserAuthorities,
+  register,
+  deleteUser
+} from '@/api/user'
+
+import { getAuthorityList } from '@/api/authority'
+import { setUserInfo, resetPassword } from '@/api/user.js'
+
+import { nextTick, ref, watch } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+const path = ref(import.meta.env.VITE_BASE_API)
+// 初始化相关
+const setAuthorityOptions = (AuthorityData, optionsData) => {
+  AuthorityData &&
+        AuthorityData.forEach(item => {
+          if (item.children && item.children.length) {
+            const option = {
+              authorityId: item.authorityId,
+              authorityName: item.authorityName,
+              children: []
+            }
+            setAuthorityOptions(item.children, option.children)
+            optionsData.push(option)
+          } else {
+            const option = {
+              authorityId: item.authorityId,
+              authorityName: item.authorityName
+            }
+            optionsData.push(option)
+          }
+        })
+}
+
+const page = ref(1)
+const total = ref(0)
+const pageSize = ref(10)
+const tableData = ref([])
+// 分页
+const handleSizeChange = (val) => {
+  pageSize.value = val
+  getTableData()
+}
+
+const handleCurrentChange = (val) => {
+  page.value = val
+  getTableData()
+}
+
+// 查询
+const getTableData = async() => {
+  const table = await getUserList({ page: page.value, pageSize: pageSize.value })
+  if (table.code === 0) {
+    tableData.value = table.data.list
+    total.value = table.data.total
+    page.value = table.data.page
+    pageSize.value = table.data.pageSize
+  }
+}
+
+watch(tableData, () => {
+  setAuthorityIds()
+})
+
+const initPage = async() => {
+  getTableData()
+  const res = await getAuthorityList({ page: 1, pageSize: 999 })
+  setOptions(res.data.list)
+}
+
+initPage()
+
+const resetPasswordFunc = (row) => {
+  ElMessageBox.confirm(
+    '是否将此用户密码重置为123456?',
+    '警告',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+    }
+  ).then(async() => {
+    const res = await resetPassword({
+      ID: row.ID,
+    })
+    if (res.code === 0) {
+      ElMessage({
+        type: 'success',
+        message: res.msg,
+      })
+    } else {
+      ElMessage({
+        type: 'error',
+        message: res.msg,
+      })
+    }
+  })
+}
+const setAuthorityIds = () => {
+  tableData.value && tableData.value.forEach((user) => {
+    const authorityIds = user.authorities && user.authorities.map(i => {
+      return i.authorityId
+    })
+    user.authorityIds = authorityIds
+  })
+}
+
+const chooseImg = ref(null)
+const openHeaderChange = () => {
+  chooseImg.value.open()
+}
+
+const authOptions = ref([])
+const setOptions = (authData) => {
+  authOptions.value = []
+  setAuthorityOptions(authData, authOptions.value)
+}
+
+const deleteUserFunc = async(row) => {
+  const res = await deleteUser({ id: row.ID })
+  if (res.code === 0) {
+    ElMessage.success('删除成功')
+    row.visible = false
+    await getTableData()
+  }
+}
+
+// 弹窗相关
+const userInfo = ref({
+  username: '',
+  password: '',
+  nickName: '',
+  headerImg: '',
+  authorityId: '',
+  authorityIds: [],
+})
+
+const rules = ref({
+  userName: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 5, message: '最低5位字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入用户密码', trigger: 'blur' },
+    { min: 6, message: '最低6位字符', trigger: 'blur' }
+  ],
+  nickName: [
+    { required: true, message: '请输入用户昵称', trigger: 'blur' }
+  ],
+  authorityId: [
+    { required: true, message: '请选择用户角色', trigger: 'blur' }
+  ]
+})
+const userForm = ref(null)
+const enterAddUserDialog = async() => {
+  userInfo.value.authorityId = userInfo.value.authorityIds[0]
+  userForm.value.validate(async valid => {
+    if (valid) {
+      const req = {
+        ...userInfo.value
+      }
+      if (dialogFlag.value === 'add') {
+        const res = await register(req)
+        if (res.code === 0) {
+          ElMessage({ type: 'success', message: '创建成功' })
+          await getTableData()
+          closeAddUserDialog()
+        }
+      }
+      if (dialogFlag.value === 'edit') {
+        const res = await setUserInfo(req)
+        if (res.code === 0) {
+          ElMessage({ type: 'success', message: '编辑成功' })
+          await getTableData()
+          closeAddUserDialog()
+        }
+      }
+    }
+  })
+}
+
+const addUserDialog = ref(false)
+const closeAddUserDialog = () => {
+  userForm.value.resetFields()
+  userInfo.value.headerImg = ''
+  userInfo.value.authorityIds = []
+  addUserDialog.value = false
+}
+
+const dialogFlag = ref('add')
+
+const addUser = () => {
+  dialogFlag.value = 'add'
+  addUserDialog.value = true
+}
+const changeAuthority = async(row, flag) => {
+  if (flag) {
+    return
+  }
+
+  await nextTick()
+  const res = await setUserAuthorities({
+    ID: row.ID,
+    authorityIds: row.authorityIds
+  })
+  if (res.code === 0) {
+    ElMessage({ type: 'success', message: '角色设置成功' })
+  }
+}
+
+const openEdit = (row) => {
+  dialogFlag.value = 'edit'
+  userInfo.value = JSON.parse(JSON.stringify(row))
+  addUserDialog.value = true
+}
+
+</script>
+
+<style lang="scss">
+.user-dialog {
+  .header-img-box {
+  width: 200px;
+  height: 200px;
+  border: 1px dashed #ccc;
+  border-radius: 20px;
+  text-align: center;
+  line-height: 200px;
+  cursor: pointer;
+}
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    border: 1px dashed #d9d9d9 !important;
+    border-radius: 6px;
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+}
+.nickName{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.pointer{
+  cursor: pointer;
+  font-size: 16px;
+  margin-left: 2px;
+}
+</style>
---
+++ src/view/mantainMemberManage/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- 维保人员管理 -->
+    <div>
+        维保人员管理
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    name: "MantainMemberManage",
+}
+</script>
+
+<script setup>
+</script>
+
